214b4f141813b53c5704d2ab0994d75f5f4b979a
DcViet <111166640+DcViet@users.noreply.github.com>
2023-04-12 11:18:02 +0700

    Update Store_Management.cs

--- Store_Management.cs
+++ Store_Management.cs
@@ -2,3 +2,32 @@
 MSSV: 218802**
 Nhập môn lập trình K2-2022-2023 (12/2022-04/2023)
 */
+// Hàm Search Item không sử dụng LINQ
+static void SearchItem()
+{
+    Console.WriteLine("Search for a product by code, name, or manufacturing company:");
+    string searchTerm = Console.ReadLine().ToLower();
+    bool foundMatch = false;
+
+    foreach (var product in products)
+    {
+        if (product.code.ToString().Contains(searchTerm) ||
+            product.name.ToLower().Contains(searchTerm) ||
+            product.manufacturingCompany.ToLower().Contains(searchTerm))
+        {
+            Console.WriteLine("Code: {0}", product.code);
+            Console.WriteLine("Name: {0}", product.name);
+            Console.WriteLine("Expiry date: {0}", product.expiryDate.ToString("dd/MM/yyyy"));
+            Console.WriteLine("Manufacturing company: {0}", product.manufacturingCompany);
+            Console.WriteLine("Production year: {0}", product.productionYear);
+            Console.WriteLine("Product type: {0}", product.productType);
+            Console.WriteLine();
+            foundMatch = true;
+        }
+    }
+
+    if (!foundMatch)
+    {
+        Console.WriteLine("No products found.");
+    }
+}
